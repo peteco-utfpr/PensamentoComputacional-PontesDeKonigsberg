2d3610fcc89bb7a8dedcea66ee03cfa7a3234435
unknown <victorgarrett@alunos.utfpr.edu.br>
2021-08-13 11:48:54 -0300

    updated character

--- include/character.h
+++ include/character.h
@@ -1,7 +1,6 @@
 #ifndef CHARACTER_H
 #define CHARACTER_H
 
-//#include "sprites.h"
 
 #include <iostream>
 #include <SFML/Graphics.hpp>
@@ -10,12 +9,7 @@
 
 #include "clickable.h"
 
-class Boat;
-
 /*
-
-class Character : public Sprites
--> Essa classe, filha da classe Sprites, é uma classe para armazenar os dados dos personagens do jogo. Onde eles estão, qual é o personagem, pricipalmente.
 
 
 VARIÁVEIS PÚBLICAS:
@@ -30,32 +24,14 @@ onde se encontra essa função.
 
 */
 class Character : public sf::Sprite, public Clickable{
-    public:
-        enum characterType{
-            priest,
-            cannibal,
-            none,
-            typeCount
-        };
 
     private:
-        bool isPriest; //False é canibal e true é missionário;
-        characterType type;
-        int location; //1 é para lado esquerdo, 2 é para o barco e 3 é no lado direito;
-        sf::Texture texture;
-        sf::Texture brighttexture; //Essa é a textura do personagem como opção;
+        sf::Texture *texture;
 
     public:
-        Character(sf::Texture* texture = NULL, characterType type = priest, int location = 0, sf::Vector2f position = {0.0f, 0.0f}, sf::Vector2f scale = {1.0f, 1.0f}); //Construtor básico da classe;
+        Character(sf::Texture* texture = NULL, sf::Vector2f position = {0.0f, 0.0f}, sf::Vector2f scale = {1.0f, 1.0f}); //Construtor básico da classe;
         virtual ~Character();
-        void moveTo (std::stack<sf::Vector2f> &leftSpaces, std::stack<sf::Vector2f> &boatSpaces, std::stack<sf::Vector2f> &rightSpaces, Boat &boat); //Essa função tem como objetivo recolocar o persongem, caso o jogador tenha clicado nele.
-        bool setBrightTexture (std::string way); //Função para configurar a textura da opção.
-        void setLoadedTexture (sf::Texture* loadedTexture);
-        void setLocation(int location);
         void setBright(bool bright);
-        void setDead(bool dead);
-        int getLocation();
-        characterType getType();
 };
 
 #endif /* CHARACTER_H */
