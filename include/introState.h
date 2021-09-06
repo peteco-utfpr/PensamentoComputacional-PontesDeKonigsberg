e6c407b840b1344fccfb658f2b028126b0212400
unknown <victorgarrett@alunos.utfpr.edu.br>
2021-09-06 13:32:56 -0300

    added intro

--- include/introState.h
+++ include/introState.h
@@ -1,8 +1,29 @@
 #include "state.h"
 
+#include "graphicsManager.h"
+#include "eventsManager.h"
+
+#define INTRO_BACKGROUND_PATH "assets/konigsberg-1581-22.jpg"
+#define INTRO_FONT_PATH "./assets/anirm__.ttf"
+#define LOGO_PETECO_PATH "./assets/LogoPETECO.png"
+
+#define LOGO_POSITION                        {610.0f, 400.0f}
+#define INTRO_TITLE_SIZE                     70
+#define INTRO_TITLE_INITIAL_POSITION         {405.0f, 520.0f}
+#define TITLE_INITIAL_HEIGHT                 520
+#define TITLE_FINAL_HEIGHT                   200
+#define STATIC_LOGO_FRAMES                   200
+#define LOGO_FADEOUT_ANIMATION_FRAMES        60
+#define TITLE_FADEIN_ANIMATION_FRAMES        120
+#define TITLE_SLIDE_ANIMATION_FRAMES         60
+
 class IntroState: public State{
 private:
+     sf::Sprite background;
+     sf::Sprite logo;
+     sf::Text title;
 
+     int frame;
 public:
      IntroState(StateMachine* pStateMachine = NULL, GraphicsManager* stateGraphicsManager = NULL, EventsManager* stateEventsManager = NULL);
      virtual ~IntroState();
--- source/introState.cpp
+++ source/introState.cpp
@@ -1,7 +1,22 @@
 #include "introState.h"
 
+IntroState::IntroState(StateMachine* pStateMachine, GraphicsManager* stateGraphicsManager, EventsManager* stateEventsManager):
+State(pStateMachine, stateGraphicsManager, stateEventsManager),
+background(*stateGraphicsManager->loadTexture(INTRO_BACKGROUND_PATH)),
+logo(*stateGraphicsManager->loadTexture(LOGO_PETECO_PATH)),
+title("Pontes de Konigsberg", *stateGraphicsManager->loadFont("./assets/anirm__.ttf"), INTRO_TITLE_SIZE){
 
-IntroState::IntroState(StateMachine* pStateMachine, GraphicsManager* stateGraphicsManager, EventsManager* stateEventsManager):State(pStateMachine, stateGraphicsManager, stateEventsManager){
+    background.setPosition(0, 0);
+    sf::Vector2f canvasSize = stateGraphicsManager->getCanvasSize();
+    sf::FloatRect backgroundRect = background.getGlobalBounds();
+    background.scale(canvasSize.x/backgroundRect.width, canvasSize.y/backgroundRect.height);
+
+    title.setPosition(INTRO_TITLE_INITIAL_POSITION);
+    title.setFillColor(sf::Color(255, 255, 255, 0));
+
+    logo.setPosition(LOGO_POSITION);
+
+    frame = 0;
 
 }
 IntroState::~IntroState(){
@@ -9,8 +24,28 @@ IntroState::~IntroState(){
 }
 
 void IntroState::update(){
-    pStateMachine->changeState(1, nullptr);
+
+    if(frame < STATIC_LOGO_FRAMES){
+
+    }
+    else if(frame < LOGO_FADEOUT_ANIMATION_FRAMES + STATIC_LOGO_FRAMES){
+        logo.setColor(sf::Color(255,255,255,255-((frame-STATIC_LOGO_FRAMES)*(255.0/(LOGO_FADEOUT_ANIMATION_FRAMES)))));
+    }
+    else if(frame < TITLE_FADEIN_ANIMATION_FRAMES + LOGO_FADEOUT_ANIMATION_FRAMES + STATIC_LOGO_FRAMES){
+        title.setColor(sf::Color(255,255,255, (frame-LOGO_FADEOUT_ANIMATION_FRAMES - STATIC_LOGO_FRAMES)*(255.0/TITLE_FADEIN_ANIMATION_FRAMES)));
+        background.setColor(sf::Color(255,255,255, 255 - (frame-LOGO_FADEOUT_ANIMATION_FRAMES - STATIC_LOGO_FRAMES)*(75.0/TITLE_FADEIN_ANIMATION_FRAMES)));
+    }
+    else{
+        title.move(0, ((float)(TITLE_FINAL_HEIGHT-TITLE_INITIAL_HEIGHT)/TITLE_SLIDE_ANIMATION_FRAMES));
+    }
+
+    frame++;
+
+    if(frame > TITLE_SLIDE_ANIMATION_FRAMES + TITLE_FADEIN_ANIMATION_FRAMES + LOGO_FADEOUT_ANIMATION_FRAMES + STATIC_LOGO_FRAMES)
+        pStateMachine->changeState(1, nullptr);
 }
 void IntroState::render(){
-
+    stateGraphicsManager->draw(GET_DRAWABLE_POINTER(background));
+    stateGraphicsManager->draw(GET_DRAWABLE_POINTER(title));
+    stateGraphicsManager->draw(GET_DRAWABLE_POINTER(logo));
 }
